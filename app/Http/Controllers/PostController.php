f1157dd3b9f7e22b24b449c256db3a40cda0c345
Daffa Aryasatya <daffaaryapp@gmail.com>
2023-10-20 19:55:54 +0700

    menambah method create dan store

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 //import Model "Post
 use App\Models\Post;
 use Illuminate\View\View;
+
+//return type redirectResponse
+use Illuminate\Http\RedirectResponse;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -17,5 +21,33 @@ class PostController extends Controller
         return view('posts.index', compact('posts'));
 	
     }
+
+    public function create(): View
+    {
+        return view('posts.create');
+    }
     
+    public function store(Request $request): RedirectResponse
+    {
+        //validate form
+        $this->validate($request, [
+        'image'     => 'required|image|mimes:jpeg,jpg,png|max:2048',
+        'title'     => 'required|min:5',
+        'content'   => 'required|min:10'
+        ]);
+        
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //create post
+        Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        //redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
 }
